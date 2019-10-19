af235a93eb41d488dd37f7dc06ae944b3a9f666d
Bogdan Lykhosherstov <bogdan@exdee.ai>
2019-10-19 12:19:33 -0600

    first version with embedded iframe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleDocsViewer from 'react-google-docs-viewer';
+
+const file = "./public/Risk Management Plan.docx"
+const type = 'docx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Here is the editable doc:</h1>
+      <div className="docsFrame">
+      <iframe src="https://docs.google.com/document/d/1j0eYjQuPr_W5JR-y4EAyBuBUXlBYA9w3tRrUKk9Sjg8/edit?usp=sharing?embedded=true">
+
+      </iframe>
+      </div>
+   
     </div>
   );
 }
